f7c51a9dbc79f9125fcbf1e9964d51cc0b965630
Luís Miguel Martins Almeida <luis93.pa@gmail.com>
2023-10-10 18:23:24 +0100

    Update README.py

--- README.py
+++ README.py
@@ -6,6 +6,6 @@ Matlab functions to evaluate the experiences and optimize PID values
 
  More info inside "Evaluate_experiences.m"
 
-$Files floor 1-4 contain the experiences data
+$Files floor contain the experiences data
 $matlab scripts .m to analyse data 
-$fulllog data files inside floor 1-4 files
+$fulllog data files inside floor files
